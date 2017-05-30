da4df05db2bcac5f488982c1b5f41b74b217495c
Paulik8 <ikhotin@mail.ru>
2017-05-30 22:05:42 +0300

    update

--- ask/paginator.py
+++ ask/paginator.py
@@ -3,12 +3,12 @@ from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 class PaginatorClass():
-    def paginate(self, data, page):
-        paginator = Paginator(data, 1)
-        try:
-            paginator = paginator.page(page)
-        except PageNotAnInteger:
-            paginator = paginator.page(1)
-        except EmptyPage:
-                paginator = paginator.page(paginator.num_pages)
-        return paginator
+	def paginate(self, data, page):
+		paginator = Paginator(data, 1)
+		try:
+			paginator = paginator.page(page)
+		except PageNotAnInteger:
+			paginator = paginator.page(1)
+		except EmptyPage:
+				paginator = paginator.page(paginator.num_pages)
+		return paginator
